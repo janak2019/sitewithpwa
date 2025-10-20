375eb4c313bd8b7061634e5c9378e45a3da577e1
htmlbasic <withjanak@gmail.com>
2025-10-20 18:48:04 +0545

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+createApp(App).mount('#app')
+
+// Correct registration for vite-plugin-pwa:
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+    plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Vite PWA',
+        short_name: 'VitePWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0078d7',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+  
 })
+
